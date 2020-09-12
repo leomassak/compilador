988747f5743f820583bb4433121a74ae5daa6918
Leonardo Massak <leoo.lcm23@hotmail.com>
2020-09-12 16:34:45 -0300

    feat: lexical file added

---
+++ src/app/utils/lexical.js
@@ -0,0 +1,146 @@
+export const TokenTable = [
+  {
+    lexeme: 'programa',
+    symbol: 'sprograma',
+  },
+  {
+    lexeme: 'inicio',
+    symbol: 'sinicio',
+  },
+  {
+    lexeme: 'fim',
+    symbol: 'sfim',
+  },
+  {
+    lexeme: 'procedimento',
+    symbol: 'sprocedimento',
+  },
+  {
+    lexeme: 'funcao',
+    symbol: 'sfuncao',
+  },
+  {
+    lexeme: 'se',
+    symbol: 'sse',
+  },
+  {
+    lexeme: 'entao',
+    symbol: 'sentao',
+  },
+  {
+    lexeme: 'senao',
+    symbol: 'ssenao',
+  },
+  {
+    lexeme: 'enquanto',
+    symbol: 'senquanto',
+  },
+  {
+    lexeme: 'faca',
+    symbol: 'sfaca',
+  },
+  {
+    lexeme: ':=',
+    symbol: 'satribuicao',
+  },
+  {
+    lexeme: 'escreva',
+    symbol: 'sescreva',
+  },
+  {
+    lexeme: 'leia',
+    symbol: 'sleia',
+  },
+  {
+    lexeme: 'var',
+    symbol: 'svar',
+  },
+  {
+    lexeme: 'inteiro',
+    symbol: 'sinteiro',
+  },
+  {
+    lexeme: 'booleano',
+    symbol: 'Sbooleano',
+  },
+  {
+    lexeme: 'identificador',
+    symbol: 'Sidentificador',
+  },
+  {
+    lexeme: '.',
+    symbol: 'Sponto',
+  },
+  {
+    lexeme: ';',
+    symbol: 'sponto_virgula',
+  },
+  {
+    lexeme: ',',
+    symbol: 'Svirgula',
+  },
+  {
+    lexeme: '(',
+    symbol: 'sabre_parenteses',
+  },
+  {
+    lexeme: ')',
+    symbol: 'sfecha_parenteses',
+  },
+  {
+    lexeme: '>',
+    symbol: 'Smaior',
+  },
+  {
+    lexeme: '>=',
+    symbol: 'Smaiorig',
+  },
+  {
+    lexeme: '=',
+    symbol: 'Sig',
+  },
+  {
+    lexeme: '<',
+    symbol: 'Smenor',
+  },
+  {
+    lexeme: '<=',
+    symbol: 'Smenorig',
+  },
+  {
+    lexeme: '!=',
+    symbol: 'Sdif',
+  },
+  {
+    lexeme: '+',
+    symbol: 'Smais',
+  },
+  {
+    lexeme: '-',
+    symbol: 'Smenos',
+  },
+  {
+    lexeme: '*',
+    symbol: 'Smult',
+  },
+  {
+    lexeme: 'div',
+    symbol: 'Sdiv',
+  },
+  {
+    lexeme: 'e',
+    symbol: 'Se',
+  },
+  {
+    lexeme: 'ou',
+    symbol: 'Sou',
+  },
+  {
+    lexeme: 'nao',
+    symbol: 'Snao',
+  },
+  {
+    lexeme: ':',
+    symbol: 'Sdoispontos',
+  },
+];
